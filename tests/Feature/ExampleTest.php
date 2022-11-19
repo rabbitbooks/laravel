67a282e468de72d1c7b80d46539075979ee0afc1
Dmitriy Polyakov <raznyj@gmail.com>
2022-11-19 13:11:53 +0700

    tests have been done

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,6 +3,9 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -14,8 +17,23 @@ class ExampleTest extends TestCase
      */
     public function test_example()
     {
-        $response = $this->get('/');
+        $response = $this->withHeaders([
+            'X-Header' => 'Value',
+        ])->post('http://laravel/admin/create', ['id' => 10]);
 
         $response->assertStatus(200);
+    }
+
+    public function test_avatars_can_be_uploaded()
+    {
+        Storage::fake('avatars');
+
+        $file = UploadedFile::fake()->image('avatar.jpg');
+
+        $response = $this->post('/avatar', [
+            'avatar' => $file,
+        ]);
+
+        Storage::disk('avatars')->assertMissing($file->hashName());
     }
 }
