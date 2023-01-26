19d29b220604a870eb1c6fef5ad9a940704e6932
Dmitriy Polyakov <catdelion@mail.ru>
2023-01-26 20:17:09 +0700

    fix

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -7,7 +7,7 @@ use SocialiteProviders\Manager\OAuth2\User as UserOAuth2;
 
 class UserRepository
 {
-    public function getUserBySocId(UserOAuth2 $user,string $socName) {
+    public function getUserBySocId(UserOAuth2 $user, string $socName) {
         $userInSystem = User::query()
             ->where('id_in_soc', $user->id)
             ->where('type_auth', $socName)
